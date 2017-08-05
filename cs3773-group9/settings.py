908a3f59471c02f999435ce33ff45f7fa2f40bc2
Autumn Salinas <aysalinas88@gmail.com>
2017-08-04 23:36:03 -0500

    testing html view of home page and making sure view shows up.

--- cs3773-group9/settings.py
+++ cs3773-group9/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# adding new lines
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
