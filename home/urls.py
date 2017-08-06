9b2c3ee3106c719126e857844693ade06c3612e5
Autumn Salinas <aysalinas88@gmail.com>
2017-08-06 17:22:29 -0500

    added background image to home page.

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from django.conf.urls import url
 from home import views
 
 urlpatterns = [
-    url(r'^$', views.HomePageView.as_view(), name='home'), # URL has been named
+    url(r'^$', views.HomePageView.as_view(), name='index'), # URL has been named
     url(r'^about/$', views.AboutPageView.as_view(), name='about'),
 ]
