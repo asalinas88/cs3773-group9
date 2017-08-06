51ca7ae3e107a0650696a8a87b14936a9c38f67d
dromero <diegor1sme@gmail.com>
2017-08-06 18:10:40 -0500

    Added a login page to home

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 #cs3773-group9/home/urls.py
 
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from home import views
 
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view(), name='index'), # URL has been named
     url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^login/$', login, {'template_name': 'login.html'}),
 ]
