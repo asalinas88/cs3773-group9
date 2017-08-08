69bf529e329ad2d11967500997b84f725e585241
Autumn Salinas <aysalinas88@gmail.com>
2017-08-08 12:17:52 -0500

    formatting changes

--- cs3773-group9/urls.py
+++ cs3773-group9/urls.py
@@ -22,7 +22,7 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
+   # url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
     url(r'^profile/', user_views.profile, name='profile'),
