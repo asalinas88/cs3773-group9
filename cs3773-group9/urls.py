5684a4663e62fc4fbb40facb599792ad6cc2bdce
Autumn Salinas <aysalinas88@gmail.com>
2017-08-08 11:46:05 -0500

    urls.py needed to be changed to a list. everything shows up. login not working yet.

--- cs3773-group9/urls.py
+++ cs3773-group9/urls.py
@@ -21,7 +21,7 @@ from users import views as user_views
 from django.contrib.auth import views as auth_views
 
 
-urlpatterns = {
+urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
@@ -30,4 +30,4 @@ urlpatterns = {
     url(r'^fullcalendar/', TemplateView.as_view(template_name="fullcalendar.html"), name='fullcalendar'),
     url(r'^schedule/', include('schedule.urls'), name='scheduler'),
     url(r'^admin/', admin.site.urls),
-}
+]
